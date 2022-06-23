8e2cb3b9cabc7df20898a00798e56df25eb2990c
Multistarkalpak <105732959+Multistarkalpak@users.noreply.github.com>
2022-06-23 12:07:49 +0530

    Simple linked list

---
+++ Linked_list/Linked_list/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace LinkedList
+{
+    public class Program
+    {
+        public static void Main(String[] args)
+        {
+            Linkedlist linked = new Linkedlist();
+            Node node1 = new Node(70);
+            //linked.AddNode(node1);
+            Node node2 = new Node(56);
+            //linked.AddNode(node2);
+            Node node3 = new Node(30);
+            //linked.AddNode(node3);
+            //linked.Display();
+            //uc2
+            //linked.AddatFirst(70);
+            //linked.AddatFirst(30);
+            //linked.AddatFirst(56);
+            //uc3
+            //linked.append(56);
+            //linked.append(30);
+            //linked.append(70);
+
+            //pritn uc2 and 3
+            //linked.printList();
+            //uc4
+            //linked.Head = null;
+            //linked.Head = new Node(56);
+            //linked.Head.next = new Node(70);
+            //Console.WriteLine("Linked list before" + "insertion:");
+            //linked.Display1();
+            //linked.InsertAtMid(30);
+            //Console.WriteLine("\nlinkedlist after "+"insertion:");
+            //linked.Display1();
+            //uc5
+            linked.AddNode(node3);
+            linked.AddNode(node2);
+            linked.AddNode(node1);
+            //Console.Write("Delete first element");
+            //linked.removeFront();
+            Console.Write("Delete last element");
+            linked.DeletingNodeAtEnd();
+            linked.Display();
+
+
+
+
+
+
+        }
+    }
+}
