0e3a57af024f5e0ed2d448e4b8e9b28eac693f65
Multistarkalpak <105732959+Multistarkalpak@users.noreply.github.com>
2022-06-23 12:16:19 +0530

    Adding the list

--- Linked_list/Linked_list/Class1.cs
+++ Linked_list/Linked_list/Class1.cs
@@ -4,9 +4,63 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Linked_list
+namespace LinkedList
 {
-    internal class Class1
+    public class Linkedlist
     {
+        public Node Head;
+        public Node Tail;
+        public Linkedlist()
+        {
+            Head = null;
+            Tail = null;
+        }
+        public void AddNode(Node node)
+        {
+            if (Head == null && Tail == null)
+            {
+                Head = node;
+                Tail = node;
+            }
+            else
+            {
+                node.next = Head;
+                Head = node;
+            }
+
+        }
+        internal void Display()
+        {
+            Node temp = Head;
+            if (temp == null)
+                Console.WriteLine("Linked list is empty");
+            else
+                Console.WriteLine("Linked list");
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data);
+                if (temp.next != null)
+                    Console.WriteLine(">");
+                temp = temp.next;
+            }
+        }
+        public void AddatFirst(int element)
+        {
+            Node newnode = new Node(element);
+            newnode.next = Head;
+            Head = newnode;
+
+
+
+        }
+        public void printList()
+        {
+            Node tnode = Head;
+            while (tnode != null)
+            {
+                Console.WriteLine(tnode.data + " ");
+                tnode = tnode.next;
+            }
+        }
     }
 }
