9fd4f5d25694d4473b29968dad30363c20846d55
Multistarkalpak <105732959+Multistarkalpak@users.noreply.github.com>
2022-06-23 12:33:48 +0530

    Delete the node at the end

--- Linked_list/Linked_list/Class1.cs
+++ Linked_list/Linked_list/Class1.cs
@@ -29,21 +29,23 @@ namespace LinkedList
             }
 
         }
+        //uc1,uc5 and uc6(print)
         internal void Display()
         {
             Node temp = Head;
             if (temp == null)
                 Console.WriteLine("Linked list is empty");
             else
-                Console.WriteLine("Linked list");
+                Console.Write("Linked list");
             while (temp != null)
             {
-                Console.WriteLine(temp.data);
+                Console.Write(temp.data);
                 if (temp.next != null)
-                    Console.WriteLine(">");
+                    Console.Write("->");
                 temp = temp.next;
             }
         }
+        //uc2
         public void AddatFirst(int element)
         {
             Node newnode = new Node(element);
@@ -53,14 +55,96 @@ namespace LinkedList
 
 
         }
+        //uc3
+        public void append(int newdata)
+        {
+            Node new_node = new Node(newdata);
+            if (Head == null)
+            {
+                Head = new Node(newdata);
+                return;
+            }
+            new_node.next = null;
+            Node last = Head;
+            while (last.next != null)
+                last = last.next;
+            last.next = new_node;
+            return;
+
+
+        }
+        //this is to print for the both uc2 and uc3
         public void printList()
         {
             Node tnode = Head;
             while (tnode != null)
             {
-                Console.WriteLine(tnode.data + " ");
+                Console.Write(tnode.data + "->");
                 tnode = tnode.next;
             }
         }
+        //uc4
+        public void InsertAtMid(int element)
+        {
+            if (Head == null)
+                Head = new Node(element);
+            else
+            {
+                Node newNode = new Node(element);
+                Node pointer1 = Head;
+                Node pointer2 = Head.next;
+                while (pointer2 != null && pointer2.next != null)
+                {
+                    pointer1 = pointer1.next;
+                    pointer2 = pointer2.next.next;
+                }
+                newNode.next = pointer1.next;
+                pointer1.next = newNode;
+            }
+
+
+        }
+        //print uc4
+        public void Display1()
+        {
+            Node temp = Head;
+            while (temp != null)
+            {
+                Console.Write(temp.data + "->");
+                temp = temp.next;
+            }
+        }
+        //uc5
+        public void removeFront()//deletingNodeAtFirst
+        {
+            if (this.Head != null)
+            {
+                Node temp = this.Head;
+                this.Head = this.Head.next;
+                temp = null;
+            }
+        }
+        public void DeletingNodeAtEnd()
+        {
+            if (this.Head != null)
+            {
+                if (this.Head.next == null)
+                {
+                    this.Head = null;
+                }
+                else
+                {
+                    Node temp = this.Head;
+
+                    while (temp.next.next != null)
+                        temp = temp.next;
+                    Node lastNode = temp.next;
+                    temp.next = null;
+                    lastNode = null;
+                }
+            }
+        }
+
+
     }
 }
